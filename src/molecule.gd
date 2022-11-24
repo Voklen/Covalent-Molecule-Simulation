35b7c8dd081f6fdd71b8119a25a98817084fc23e
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-11-24 08:49:54 +0000

    Forces working

--- src/molecule.gd
+++ src/molecule.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var speed = 0.5
+var speed = 50
 
 func _ready():
 	pass
@@ -13,7 +13,7 @@ func get_positive():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var force_on_negative = Vector2()
+	var global_force_on_negative = Vector2()
 	var force_on_positive = Vector2()
 	var other_nodes = get_parent().get_children()
 	for node in other_nodes:
@@ -21,17 +21,19 @@ func _process(delta):
 #			Negative repulsion
 			var neg_neg_displacement = get_negative() - node.get_negative()
 			var neg_neg_force = displacement_to_force(neg_neg_displacement)
-			force_on_negative += neg_neg_force
+			global_force_on_negative += neg_neg_force
 #			Negative attraction
-			var neg_pos_displacement = get_negative() - node.get_positive()
-			var neg_pos_force = displacement_to_force(neg_pos_displacement)
-			force_on_negative -= neg_pos_force
-	force_on_negative = force_on_negative * delta * speed
-	var combined = get_node("Negative").position + force_on_negative
-	rotation += combined.angle() * delta * speed
-	var new_position = get_negative().move_toward(to_global(combined), 0.5)
-	position = self.position.move_toward(new_position, delta * speed)
-	print(position)
+#			var neg_pos_displacement = get_negative() - node.get_positive()
+#			var neg_pos_force = displacement_to_force(neg_pos_displacement)
+#			force_on_negative -= neg_pos_force
+	global_force_on_negative *= delta * speed
+	var local_combined_position = get_node("Negative").position + global_force_on_negative
+	print(local_combined_position)
+	var new_rotation = get_node("Negative").position.angle_to(local_combined_position)
+	rotate(new_rotation)
+	Vector2().angle()
+	position += get_node("Negative").position.move_toward(local_combined_position, 0.5) - get_node("Negative").position
+#	position = self.position.move_toward(new_position, delta * speed)
 	
 
 func displacement_to_force(displacement: Vector2):
